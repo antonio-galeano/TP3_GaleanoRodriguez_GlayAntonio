b876eee186a33763fbe35ffae7aab3fbe77e9210
antonio-galeano <126198500+antonio-galeano@users.noreply.github.com>
2024-08-12 10:30:49 -0400

    Ajustements dans toutes les pages pour améliorer la réactivité et l'adaptabilité.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-const menuButton = document.querySelector('button[aria-controls="mobile-menu"]');
-const mobileMenu = document.getElementById('mobile-menu');
-const closeButton = document.getElementById('close-menu');
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.querySelector('button[aria-controls="mobile-menu"]');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const mainContent = document.querySelector('.main-content-section');
 
-menuButton.addEventListener('click', () => {
-    const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
-    menuButton.setAttribute('aria-expanded', !isExpanded);
-    mobileMenu.classList.toggle('hidden');
+    menuButton.addEventListener('click', function () {
+        const isExpanded = this.getAttribute('aria-expanded') === 'true';
+        this.setAttribute('aria-expanded', !isExpanded);
+        mobileMenu.classList.toggle('hidden');
+        mainContent.classList.toggle('mt-40');
+    });
+
+    const closeButton = document.getElementById('close-menu');
+    closeButton.addEventListener('click', function () {
+        mobileMenu.classList.add('hidden');
+        mainContent.classList.remove('mt-40');
+    });
 });
 
-closeButton.addEventListener('click', () => {
-    mobileMenu.classList.add('hidden');
-});
+
 
 
 
